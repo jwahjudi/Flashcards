2115d046f7d311550ead4508182f37dd76777b15
Jason Wahjudi <jasonwahjudi@yahoo.com>
2018-10-13 13:40:00 -0700

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Jason Wahjudi on 10/13/18.
+//  Copyright © 2018 Jason Wahjudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
